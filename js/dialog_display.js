b577f1ca3a2b97807f7713206860209c858e5d80
Prawin <theincredible13@gmail.com>
2017-03-02 06:34:43 +0530

    first

---
+++ js/dialog_display.js
@@ -0,0 +1,17 @@
+   $(document).ready(function(){
+   $('.certifications').click(function(){
+ BootstrapDialog.show({
+ 			title : 'Certifications',
+            message: 'Get insight view of latest trends across wide range of Categories ',
+                  
+});
+});
+  $('.contact').click(function(){
+ BootstrapDialog.show({
+ 	title : 'Contact',
+            
+            message: 'For Queries and Support write to us at :\n\n support@tencyclopedia.com',
+                  
+});
+});
+});
